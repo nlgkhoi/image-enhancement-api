6f609cd759806346dd100367b766f9339d0561d1
Andy <tauit.dnmd@gmail.com>
2021-09-08 23:04:10 +0700

    update config

--- module/application_container.py
+++ module/application_container.py
@@ -17,5 +17,7 @@ class ApplicationContainer(containers.DeclarativeContainer):
     enhance_service = providers.Singleton(
         EnhanceService,
         deblur_model_path=config.deblur_model_path,
-        use_cpu=config.run_on_cpu
+        use_cpu=config.run_on_cpu,
+        use_deblur_model=config.use_deblur_model,
+        enhanced_output_only=config.enhanced_output_only,
     )
